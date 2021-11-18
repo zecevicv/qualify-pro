7044ba077bfb61636c81448cd51485a053c3a133
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-11-18 15:37:31 +0100

    [ADD] - Homepage

---
+++ js/app.js
@@ -0,0 +1,55 @@
+/* #Banner Slider
+  ======================================================= */
+if (document.querySelector('.banner-slider')) {
+  new Swiper(".banner-slider .swiper", {
+    slidesPerView: 1,
+    pagination: {
+      el: ".banner-slider .swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+}
+
+/* #Product Slider
+  ======================================================= */
+if (document.querySelector('.product-slider')) {
+  new Swiper(".product-slider .swiper", {
+    breakpoints: {
+      0: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 5
+      }
+    },
+    navigation: {
+      nextEl: ".product-slider .next",
+      prevEl: ".product-slider .prev",
+    },
+    pagination: {
+      el: ".product-slider .swiper-pagination",
+    },
+  });
+}
+
+/* #Post Slider
+  ======================================================= */
+if (document.querySelector('.post-slider')) {
+  new Swiper(".post-slider .swiper", {
+    breakpoints: {
+      0: {
+        slidesPerView: 1.2,
+        loop: true,
+        centeredSlides: true,
+      },
+      1024: {
+        slidesPerView: 3,
+        loop: false,
+        centeredSlides: false
+      }
+    },
+  });
+}
